df379eab8caa1c744412c66836efdbb6dc2c5159
neillane <neillane.rosario@ucsal.edu.br>
2025-10-16 20:24:16 -0300

    style: add Header

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
+import Header from "./../components/Header"
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -18,8 +18,9 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="pt-BR">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <Header />
         {children}
       </body>
     </html>
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
+import styles from "./page.module.css";
+import Header from "../components/Header";
 
 function Home() {
   return (
-    <div>
-      <h1>Welcome to the Home Page</h1>
+    <div className={styles.container}>
+      <div className={styles.main}>
+        <h1>Welcome to the Home Page</h1>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
