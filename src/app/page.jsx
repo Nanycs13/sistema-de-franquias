b2f6ade6b435d8c093b297ed5048f3d33b3eee7e
neillane <neillane.rosario@ucsal.edu.br>
2025-10-30 20:56:01 -0300

    feat: pagehome

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,15 +1,71 @@
-import React from "react";
-import styles from "./page.module.css";
-import Header from "../components/Header";
+'use client'
 
-function Home() {
+import { Card, Row, Col } from 'antd'
+import { UserOutlined, ShopOutlined, DashboardOutlined } from '@ant-design/icons'
+import Link from 'next/link'
+import styles from './page.module.css'
+
+export default function Home() {
+
   return (
     <div className={styles.container}>
-      <div className={styles.main}>
-        <h1>Welcome to the Home Page</h1>
-      </div>
-    </div>
-  );
-}
 
-export default Home;
+      <div className={styles.header}>
+        <h1 className={styles.title}>Sistema de Franquias</h1>
+        <p className={styles.subtitle}>Gerencie suas franquias e funcionários</p>
+      </div>
+
+      <div className={styles.cardsContainer}>
+        <Row gutter={[24, 24]} justify="center">
+
+          <Col xs={24} sm={12} lg={8}>
+            <Link href="/funcionarios">
+              <Card
+                className={styles.menuCard}
+                hoverable
+              >
+                <div className={styles.cardContent}>
+                  <UserOutlined className={`${styles.cardIcon} ${styles.funcionariosIcon}`} />
+                  <h3 className={styles.cardTitle}>Funcionários</h3>
+                  <p className={styles.cardDescription}>Gerenciar funcionários das franquias</p>
+                </div>
+              </Card>
+            </Link>
+          </Col>
+
+          <Col xs={24} sm={12} lg={8}>
+            <Link href="/franquias">
+              <Card
+                className={styles.menuCard}
+                hoverable
+              >
+                <div className={styles.cardContent}>
+                  <ShopOutlined className={`${styles.cardIcon} ${styles.franquiasIcon}`} />
+                  <h3 className={styles.cardTitle}>Franquias</h3>
+                  <p className={styles.cardDescription}>Gerenciar franquias da empresa</p>
+                </div>
+              </Card>
+            </Link>
+          </Col>
+
+          <Col xs={24} sm={12} lg={8}>
+            <Link href="/dashboard">
+              <Card
+                className={styles.menuCard}
+                hoverable
+              >
+                <div className={styles.cardContent}>
+                  <DashboardOutlined className={`${styles.cardIcon} ${styles.dashboardIcon}`} />
+                  <h3 className={styles.cardTitle}>Dashboard</h3>
+                  <p className={styles.cardDescription}>Relatórios e indicadores (B.I)</p>
+                </div>
+              </Card>
+            </Link>
+          </Col>
+
+        </Row>
+      </div>
+
+    </div>
+  )
+}
